0863e857ca5d9e5cf6935dda4a51fb7dd4907d23
victorvictorvk <lavialactea89@hotmial.com>
2019-02-19 09:04:17 +0100

    subida

---
+++ ejercicio3.java
@@ -0,0 +1,26 @@
+package ejerciciosTema1;
+
+import java.util.Scanner;
+
+public class ejercicio3 {
+
+	public static void main(String[] args) {
+		int aActual;     
+	      int aNacimiento; 
+	      int edad;
+	      Scanner sc = new Scanner(System.in);
+
+	      
+	      System.out.print("Año de nacimiento: ");
+	      aNacimiento = sc.nextInt();
+	      System.out.print("Año actual: ");
+	      aActual = sc.nextInt();
+
+	      edad = aActual - aNacimiento; 
+
+	      System.out.println("Su edad es: " + edad + " años.");
+
+
+	}
+
+}
